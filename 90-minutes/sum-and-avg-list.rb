4864a3ee94e948f2136d7d941eb75ef2439a3a67
Kenneth G. Kang <kenneth.g.kang@vanderbilt.edu>
2017-01-25 14:24:23 -0600

    working on 90 minutes code challenge

---
+++ 90-minutes/sum-and-avg-list.rb
@@ -0,0 +1,23 @@
+# sum and avgs from a list, but only the odd values from said list
+# sum_and_avg([1,2,3,4]) should return: {sum: 4, avg: 2}
+
+# done in <5 min
+
+def sum_and_avg(given_array)
+  counter = 0
+  sum = 0
+  avg = 0
+  until counter >= given_array.length
+    if (counter % 2 == 0)
+      sum += given_array[counter]
+    end
+    counter += 1
+  end
+  avg = sum/(counter/2).to_f
+  puts "Sum is: #{sum}"
+  puts "Avg is: #{avg}"
+end
+
+
+
+sum_and_avg([1,2,3,4,5]) #correct
